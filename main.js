94fed5a34f383c39d0d856ce016ea2fcb487629a
Qosim Aisha <qosimaishaoyindamola@gmail.com>
2023-11-26 03:52:11 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function clik(){
+    let a = document.getElementById('p')
+    a.classList.toggle("active")
+}
+
+function log(){
+    let log = document.getElementById('text')
+    log.classList.toggle("hide")
+}
